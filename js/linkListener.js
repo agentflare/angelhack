39c025fdee27092c781f7511db4f33201746562b
agentflare <samuel.wu.u1@gmail.com>
2015-06-14 11:24:26 -0400

    Changes front end succesfully

--- js/linkListener.js
+++ js/linkListener.js
@@ -1,21 +1,11 @@
-function setUpNextPage(){
+function setUpNextPage(data){
 	
 	$("#loadingImage").css("display","none");
-	
-	$.get(rssurl, function(data) {
-		var $xml = $(data);
-		$xml.find("item").each(function() {
-			var $this = $(this),
-				item = {
-					title: $this.find("title").text(),
-					link: $this.find("link").text(),
-					description: $this.find("description").text(),
-					pubDate: $this.find("pubDate").text(),
-					author: $this.find("author").text()
-			}
-			alert(item.title);
-		});
-	});
+	$("#a2").html("<h2>"+data.data[0].title+"</h2> <p>"+data.data[0].pubDate+"</p>");
+	$("#a3").html("<h2>"+data.data[1].title+"</h2> <p>"+data.data[1].pubDate+"</p>");
+	$("#a4").html("<h2>"+data.data[2].title+"</h2> <p>"+data.data[2].pubDate+"</p>");
+	$(".article-container").css("display","block");
+	$(".main").css("display","none");
 }
 
 $("#submitLink").click(function() {
@@ -28,9 +18,9 @@ $("#submitLink").click(function() {
             fileData: $("#my-awesome-dropzone").val()
         },
         success: function(data) {
-			//alert(data.concepts[0].concept);
+			alert(data.data[0].title);
 			
-			setUpNextPage();
+			setUpNextPage(data);
 			//$("#test").append(obj.message);
         },
         error: function(jqXHR, textStatus, errorThrown) {
